5830861ffe4a91ac3942d507fbd17c3922eb19f8
hantonette <habalajadia23-0250@cca.edu.ph>
2025-04-16 10:35:51 +0800

    Add files via upload

---
+++ portfolio_js.js
@@ -0,0 +1,4 @@
+document.getElementById("contactForm").addEventListener("submit", function(event) {
+    event.preventDefault(); // Prevents page reload
+    alert("Thanks! Your message is most appreciated ❤️"); // Custom alert message
+});
